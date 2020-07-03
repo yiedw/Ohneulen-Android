e1c27b1047f1e1ec0b010c83924f2c78d1698ae5
yiedw <42017660+yiedw@users.noreply.github.com>
2020-07-03 18:03:18 +0900

    나머지 화면

--- app/src/main/java/com/goodchoice/android/ohneulen/ui/home/HomeAppBarFragment.kt
+++ app/src/main/java/com/goodchoice/android/ohneulen/ui/home/HomeAppBarFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.goodchoice.android.ohneulen.R
 import com.goodchoice.android.ohneulen.databinding.HomeAppbarFragmentBinding
+import com.goodchoice.android.ohneulen.ui.mypage.MyPageAppBarFragment
 import com.goodchoice.android.ohneulen.ui.mypage.MyPageFragment
 import com.goodchoice.android.ohneulen.ui.noti.NotiAppBarFragment
 import com.goodchoice.android.ohneulen.ui.noti.NotiFragment
@@ -41,6 +42,7 @@ class HomeAppBarFragment :Fragment(){
     }
 
     fun myClick(view: View){
+        replaceAppbarFragment(MyPageAppBarFragment.newInstance())
         replaceMainFragment(MyPageFragment.newInstance())
     }
 
--- app/src/main/java/com/goodchoice/android/ohneulen/ui/mypage/MyPageAppBarFragment.kt
+++ app/src/main/java/com/goodchoice/android/ohneulen/ui/mypage/MyPageAppBarFragment.kt
@@ -1,4 +1,47 @@
 package com.goodchoice.android.ohneulen.ui.mypage
 
-class MyPageAppBarFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.goodchoice.android.ohneulen.R
+import com.goodchoice.android.ohneulen.databinding.MypageAppbarFragmentBinding
+import com.goodchoice.android.ohneulen.ui.home.HomeAppBarFragment
+import com.goodchoice.android.ohneulen.ui.home.HomeFragment
+import com.goodchoice.android.ohneulen.util.replaceAppbarFragment
+import com.goodchoice.android.ohneulen.util.replaceMainFragment
+
+class MyPageAppBarFragment : Fragment() {
+    companion object {
+        fun newInstance() = MyPageAppBarFragment()
+    }
+
+    private lateinit var binding: MypageAppbarFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.mypage_appbar_fragment,
+            container,
+            false
+        )
+        binding.fragment = this
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+    fun backClick(view: View){
+        replaceAppbarFragment(HomeAppBarFragment.newInstance())
+        replaceMainFragment(HomeFragment.newInstance())
+    }
 }
