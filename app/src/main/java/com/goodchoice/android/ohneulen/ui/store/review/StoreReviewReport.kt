f7140d372f0a5702a34d9c870b41646f63e7dac4
yiedw <42017660+yiedw@users.noreply.github.com>
2020-08-14 10:41:55 +0900

    v0.3.4-alpha
    storeReviewReport 뒤에 터치되는 버그수정

--- app/src/main/java/com/goodchoice/android/ohneulen/ui/store/review/StoreReviewReport.kt
+++ app/src/main/java/com/goodchoice/android/ohneulen/ui/store/review/StoreReviewReport.kt
@@ -1,9 +1,11 @@
 package com.goodchoice.android.ohneulen.ui.store.review
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -14,7 +16,7 @@ import com.goodchoice.android.ohneulen.databinding.ReviewReportBinding
 import com.goodchoice.android.ohneulen.ui.store.StoreViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class StoreReviewReport(private val review:Review) : Fragment() {
+class StoreReviewReport(private val review: Review) : Fragment() {
     companion object {
         fun newInstance(review: Review) = StoreReviewReport(review)
     }
@@ -34,12 +36,21 @@ class StoreReviewReport(private val review:Review) : Fragment() {
             false
         )
         binding.viewModel = storeViewModel
-        binding.review=this.review
+        binding.review = this.review
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //뒤에 레이아웃에 터치 안먹게하기
+        binding.reviewReport.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                return true
+            }
+
+        })
+
         binding.reviewReportDetail.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
 
