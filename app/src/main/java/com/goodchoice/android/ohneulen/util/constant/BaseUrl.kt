68bc2b14a0d4edd54ed3bad70abdd72b493edb83
yiedw <42017660+yiedw@users.noreply.github.com>
2020-08-24 17:10:25 +0900

    v0.7.2-alpha
    review write 배경색 안나타나게 수정

--- app/src/main/java/com/goodchoice/android/ohneulen/util/constant/BaseUrl.kt
+++ app/src/main/java/com/goodchoice/android/ohneulen/util/constant/BaseUrl.kt
@@ -2,6 +2,6 @@ package com.goodchoice.android.ohneulen.util.constant
 
 object BaseUrl{
     const val KAKAO_MAP = "https://dapi.kakao.com/"
-    const val OHNEULEN = "http://develop.ohneulen.com/"
+    const val OHNEULEN = "https://develop.ohneulen.com/"
     const val OHNEULEN_SIGN_UP="https://devm.ohneulen.com/member/regist"
 }
